e64ffb8c3a5dbe54ef48624aef48527656086e15
apriori302 <apriori302@gmail.com>
2015-09-09 15:10:42 +0800

    Initial checkin of ToolCommon

---
+++ ToolCommon/src/com/abc/demo/Main.java
@@ -0,0 +1,13 @@
+package com.abc.demo;
+ 
+public class Main {
+	public static void main(String[] args) {
+		showMessage();
+		MessageBox mb = new MessageBox();
+		mb.alert();
+	}
+	public static void showMessage(){
+		System.out.println("This Message 1");
+		System.out.println("This Message 2");
+	}
+}
---
+++ ToolCommon/src/com/abc/demo/MessageBox.java
@@ -0,0 +1,7 @@
+package com.abc.demo;
+
+public class MessageBox {
+	public void alert(){
+		System.out.println("This is an alert message.");
+	}
+}
